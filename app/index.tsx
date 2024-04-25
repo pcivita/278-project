59dbc2520e2f56e5245392d3ab349059848a5d9b
pcivita <pcivita@stanford.edu>
2024-04-25 16:31:17 -0700

    Login Template

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 const Page = () => {
   const [email, setEmail] = useState("");
@@ -13,6 +20,29 @@ const Page = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>278 Woohoo</Text>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="john@doe.com"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.inputField}
+      />
+      <TextInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.inputField}
+      />
+
+      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+        <Text style={{ color: "#fff" }}>Sign in</Text>
+      </TouchableOpacity>
+      <Button
+        onPress={onSignUpPress}
+        title="Create Account"
+        color={"#fff"}
+      ></Button>
     </View>
   );
 };
@@ -28,6 +58,23 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: "white",
     textAlign: "center",
+  },
+  inputField: {
+    marginVertical: 10,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#2b825b",
+    borderRadius: 4,
+    padding: 10,
+    color: "#fff",
+    backgroundColor: "#363636",
+  },
+  button: {
+    marginVertical: 15,
+    alignItems: "center",
+    backgroundColor: "#2b825b",
+    padding: 12,
+    borderRadius: 4,
   },
 });
 
