c1a464a1d7c0a1eb681781a6c3a2923de8cfc4da
pcivita <pcivita@stanford.edu>
2024-05-13 14:36:30 -0700

    Clean Up

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -7,17 +7,14 @@ import FriendRequestButton from "@/components/FriendRequestButton";
 const Calendar = () => {
   const [data, setData] = useState("hello");
 
- 
-
   return (
     <View style={styles.container}>
-
       <Text>Calendar 123 Screen</Text>
 
-      <FriendRequestButton
+      {/* <FriendRequestButton
         userOneId="f207cd61-2d7f-4ccb-9130-de3aed61b921"
         userTwoId="fc720675-a19e-477f-8780-328b154a278a"
-      />
+      /> */}
     </View>
   );
 };
