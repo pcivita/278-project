e64ef9f7638222642a0ddff9ed7306a0c17824ee
pcivita <pcivita@stanford.edu>
2024-05-30 11:03:18 -0700

    Revert "Merge pull request #40 from pcivita/elena-branch"
    This reverts commit c6ede4d579b0e0ceec4cf3977314b801e195f1e3, reversing
    changes made to b75a737b9cb232646c8fb4dc77459dea7c3a3e27.

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -16,7 +16,7 @@ interface Event {
   max_people: number;
   signups: number;
   current_signups: number;
-  //group_id: string;
+  group_id: string;
   creator_id: string;
   isAttending: boolean;
   event_date: string;
