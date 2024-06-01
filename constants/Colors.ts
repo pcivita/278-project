97260b42e510098ce2bb426932786f9f922bc328
malinac02 <malinacalarco@gmail.com>
2024-06-01 16:48:12 -0700

    move around colors for better feed

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,25 +8,25 @@ interface ColorSchemes {
 }
 
 const Colors: ColorSchemes = {
-  color1: {  // Pink
-    dark: "#B3578A",
-    light: "#FFE5F3",
-  },
-  color2: {  // Moss
-    dark: "#7B917A",
-    light: "#E7F0E6",
-  },
-  color3: {  // Lavender
-    dark: "#B58FBF",
-    light: "#F9EBFD",
-  },
-  color4: {  // Purple
+  color1: {  // Purple
     dark: "#757BBA",
     light: "#E6E8FF",
   },
-  color5: {  // Blue
+  color2: {  // Moss
+    dark: "#7B917A",
+    light: "#E0EDDF",
+  },
+  color3: {  // Lavender
+    dark: "#B58FBF",
+    light: "#F0E2F3",
+  },
+  color4: {  // Blue
     dark: "#5A8CAD",
-    light: "#E1F3FF",
+    light: "#DBF1FF",
+  },
+  color5: {  // Pink
+    dark: "#B3578A",
+    light: "#FFE5F3",
   },
 };
 
