3f2668d06f158f8dd5ee39f7b265cc194061145f
malinac02 <malinacalarco@gmail.com>
2024-06-02 15:32:28 -0700

    create custom monotextinput componenet

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text, TextProps } from 'react-native';
+import { TextInput, TextInputProps, Text, TextProps } from 'react-native';
 
 interface CustomTextProps extends TextProps {
   useMono?: boolean;
   useUltra?: boolean;
-  useMedium?: boolean; // Add the useMedium prop
+  useMedium?: boolean;
 }
 
 export function MonoText({ style, useMono, useUltra, useMedium, ...otherProps }: CustomTextProps) {
@@ -14,10 +14,37 @@ export function MonoText({ style, useMono, useUltra, useMedium, ...otherProps }:
   } else if (useUltra) {
     fontFamily = 'TripSans-Ultra';
   } else if (useMedium) {
-    fontFamily = 'TripSans-Medium'; // Use TripSans-Medium when useMedium is true
+    fontFamily = 'TripSans-Medium';
   }
 
-  const textStyle = { fontFamily, ...style };  // Combine fontFamily with existing styles safely
+  const textStyle = { fontFamily, ...style };
 
   return <Text {...otherProps} style={textStyle} />;
+}
+
+interface CustomTextInputProps extends TextInputProps {
+  useMono?: boolean;
+  useUltra?: boolean;
+  useMedium?: boolean;
+}
+
+export function MonoTextInput({
+  style,
+  useMono,
+  useUltra,
+  useMedium,
+  ...otherProps
+}: CustomTextInputProps) {
+  let fontFamily = 'TripSans-Regular';
+  if (useMono) {
+    fontFamily = 'SpaceMono';
+  } else if (useUltra) {
+    fontFamily = 'TripSans-Ultra';
+  } else if (useMedium) {
+    fontFamily = 'TripSans-Medium';
+  }
+
+  const textInputStyle = { fontFamily, ...style };
+
+  return <TextInput {...otherProps} style={textInputStyle} />;
 }
