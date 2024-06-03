d1b5328f702628b5218b8fc694f7089f0770042f
malinac02 <malinacalarco@gmail.com>
2024-06-03 13:23:21 -0700

    style login screen to match sign up screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,56 +1,81 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { View, TouchableOpacity, StyleSheet, Dimensions, ActivityIndicator } from "react-native";
+import Colors from "@/constants/Colors";
+import * as ImagePicker from 'expo-image-picker';
+import { Ionicons } from '@expo/vector-icons';
+import { MonoText, MonoTextInput } from "@/components/StyledText";
+import { supabase } from "@/utils/supabase";
 
 interface LoginProps {
-  navigation: any;
+  setCurrentScreen: (screen: string) => void;
 }
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const Login: React.FC<LoginProps> = ({ navigation }) => {
-  const [email, setEmail] = useState<string>("");
+const Login: React.FC<LoginProps> = ({ setCurrentScreen }) => {
+
+  const [emailOrUsername, setEmailOrUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const handleLogIn = () => {
-    console.log("login with username");
-    // logIn(email, password);
-  };
+  
+  const isFormValid = emailOrUsername !== "" && password != "";
+  const wrongLogin = true; // TODO: 
+
+  
+  const [loading, setLoading] = useState(false);
+  const onLoginPress = async () => {
+    setLoading(true);
+    
+  }
 
   return (
     <View style={styles.container}>
+      {loading && (
+        <View style={styles.overlay}>
+          <ActivityIndicator  size="large" color="white" />
+          <MonoText style={styles.overlayText}>Loading...</MonoText>
+        </View>
+      )}
       <TouchableOpacity
-        onPress={() => navigation.goBack()}
+        onPress={() => setCurrentScreen("log in options")}
         style={styles.backCaret}
       >
-        <FontAwesome5 name="arrow-left" size={24} color="black" />
-      </TouchableOpacity>
-      <View style={styles.spacing} />
-      <Text>flock logo</Text>
-      <Text style={styles.title}>Flock</Text>
-      <Text style={styles.subtitle}>
-        Hang out with your friends
-      </Text>
+        <Ionicons name="chevron-back" size={24} color="black" />
+      </TouchableOpacity>  
+      <View style={styles.header}>
+        <MonoText useUltra={true} style={styles.headerText}>Log in to your account</MonoText>
+      </View>
       <View style={styles.inputContainer}>
-        <TextInput
+        <MonoText style={styles.text}>Email or username
+          <MonoText style={styles.pinkText}> *</MonoText>
+        </MonoText>
+        <MonoTextInput
           style={styles.input}
-          placeholder="Email"
-          onChangeText={(text) => setEmail(text.toLowerCase())}
-          value={email}
+          placeholder="Email or username"
+          value={emailOrUsername}
+          onChangeText={setEmailOrUsername}
         />
-        <TextInput
+                <MonoText style={styles.text}>Choose password
+          <MonoText style={styles.pinkText}> *</MonoText>
+        </MonoText>
+        <MonoTextInput
           style={styles.input}
           placeholder="Password"
           secureTextEntry
-          onChangeText={setPassword}
           value={password}
+          onChangeText={setPassword}
         />
+        {wrongLogin && (
+          <MonoText style={styles.redText}>The email/username or password you entered is incorrect.</MonoText>
+        )}
       </View>
-      
-      <View style={{ height: 32 }} />
-      <TouchableOpacity onPress={handleLogIn} style={styles.secondaryButton}>
-        <Text style={styles.loginText}>Log In</Text>
+      <TouchableOpacity 
+        style={[styles.button, !isFormValid && { backgroundColor: Colors.color2.light }]}
+        disabled={!isFormValid}
+        onPress={onLoginPress}
+      >
+        <MonoText style={styles.buttonText}>Create Account</MonoText>
       </TouchableOpacity>
     </View>
   );
@@ -60,58 +85,89 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start",
-    padding: 20,
+    // padding: 20,
     backgroundColor: "white",
-  },
-  logo: {
-    width: 50,
-    height: 50,
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: "bold",
-    marginTop: 16,
-  },
-  subtitle: {
-    fontSize: 20,
-    marginTop: 14,
-    width: windowWidth * 0.7,
-    textAlign: "center",
-  },
-  inputContainer: {
-    width: "95%",
-    paddingVertical: 40,
-  },
-  input: {
-    width: "100%",
-    borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 5,
-  },
-  loginText: {
-    fontSize: 16,
-    color: "white",
-  },
-  secondaryButton: {
-    backgroundColor: "pink",
-    padding: 10,
-    borderRadius: 999,
-    width: windowWidth * 0.8,
-    alignItems: "center",
-    marginTop: 12,
   },
   backCaret: {
     alignSelf: "flex-start",
+    marginTop: 70,
+    marginLeft: 10,
+  },
+  header: {
+    marginTop: 100,
+    width: "90%"
+  },
+  headerText: {
+    fontSize: 24,
+  },
+  photoSelection: {
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 75,
+    marginBottom: 5,
+  },
+  greenText: {
+    color: Colors.color2.dark,
+    fontSize: 14,
+    marginBottom: 20,
+  },
+  pinkText: {
+    color: Colors.color5.dark,
+    fontSize: 16,
+  },
+  text: {
+    fontSize: 16,
+    bottom: 2,
+  },
+  redText: {
+    color: "red",
+  },
+  inputContainer: {
+    width: "90%",
     marginTop: 50,
-    width: windowWidth * 0.9,
-    height: 50,
+    height: 200,
   },
-  spacing: {
-    height: windowHeight * 0.05,
+  input: {
+    fontSize: 16,
+    width: "100%",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 5,
+    marginBottom: 10,
   },
+  button: {
+    width: windowWidth * 0.8,
+    height: 40,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.color2.dark,
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+    elevation: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    gap: 5,
+    paddingTop: 100,
+  },
+  overlayText: {
+    color: "white",
+    fontSize: 16,
+  }
 });
+
 
 export default Login;
